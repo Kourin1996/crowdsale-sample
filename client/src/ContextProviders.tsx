b458fd0d00d9cfc76fe91a8006d8d20e4dd65faa
kourin <kourin1996@gmail.com>
2021-02-16 22:38:23 +0900

    remove unused code

--- client/src/ContextProviders.tsx
+++ client/src/ContextProviders.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { EthersProviderContextProvider } from './contexts/ethers-provider'
-
 const { theme } = require('rimble-ui')
 
 export const ContextProviders: React.FC<{}> = ({ children }) => {
--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -8,17 +8,6 @@ declare global {
 }
 
 const IndexPage: React.FC<{}> = () => {
-  // React.useEffect(() => {
-  //   provider
-  //     .getBalance('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266')
-  //     .then((balance) => {
-  //       console.log('account balance', balance.toString())
-  //     })
-  //     .catch((error) => {
-  //       console.error(error)
-  //     })
-  // }, [provider])
-
   return <IndexPageTemplate />
 }
 
